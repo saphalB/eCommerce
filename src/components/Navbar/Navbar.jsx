007cd66270939a27d5c2d252498837a572147e6d
saphalB <saphalbhattarai5@gmail.com>
2025-09-02 15:00:34 +0545

    undo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -68,11 +68,7 @@ const Navbar = ({ handleOrderPopup }) => {
               Creatix Universe
             </a>
           </div>
-          {/*mobile search icon */}
-          <div className="sm:hidden">
-            <IoMdSearch className="text-gray-500 text-xl" />
-          </div>
-          {/* desktop search bar */}
+          {/* search bar */}
           <div className="flex justify-between items-center gap-4 ">
             <div className="relative group hidden sm:block">
               <input
